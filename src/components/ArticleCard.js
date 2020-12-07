56f8fa1ebfd07c218352f128137469876223c5c2
chrl0t <charlotte_i@live.co.uk>
2020-12-07 16:15:42 +0000

    added date formatter

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { format } from 'date-fns';
 
 const ArticleCard = (props) => {
   const { topic, author, created_at, votes, title, article_id } = props;
@@ -7,15 +8,17 @@ const ArticleCard = (props) => {
     <div className='article-card'>
       <header className='article-card-header'>
         <b>topic:</b> {topic} <b>user:</b> {author} <b>date: </b>
-        {created_at} <b>votes:</b> {votes}
+        {format(new Date(created_at), 'dd/MM/yyyy')} <b>votes:</b> {votes}
       </header>
       <div>
         <Link className='article-card-title' to={`/${article_id}`}>
           <h1>{title}</h1>
         </Link>
       </div>
-      <footer className='article-card-footer'>
-        <b>comments</b>
+      <footer>
+        <Link className='article-card-footer' to={`/${article_id}`}>
+          <b>comments</b>
+        </Link>
       </footer>
     </div>
   );
