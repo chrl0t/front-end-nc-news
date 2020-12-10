e5b45bd02d776501d0f2d86f951d7114740bce01
chrl0t <charlotte_i@live.co.uk>
2020-12-10 11:18:13 +0000

    added styling to comment box

--- src/components/CommentAdder.js
+++ src/components/CommentAdder.js
@@ -23,14 +23,14 @@ class CommentAdder extends React.Component {
     const { body } = this.state;
     return (
       <form className='comment-form' onSubmit={this.handleSubmit}>
-        <input
+        <textarea
           className='comment-input-box'
           type='text'
           name='body'
           id='body'
           onChange={this.handleChange}
           value={body}
-        />
+        ></textarea>
         <br></br>
         <input type='submit' className='sort-button' value='Add Comment' />
       </form>
