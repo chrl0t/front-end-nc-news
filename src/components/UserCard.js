691712da8867c5947068124247f2cd140d02efa8
chrl0t <charlotte_i@live.co.uk>
2020-12-10 16:30:37 +0000

    added some styling to userCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,10 +3,20 @@ import React from 'react';
 const UserCard = (props) => {
   const { username, name, avatar_url } = props;
   return (
-    <div className='user-card'>
-      <header className='user-card-header'>
-        <h1>{username}</h1>
-      </header>
+    <div className='user-card-page'>
+      <div className='user-card'>
+        <header className='user-card-header'>
+          <h1>{username}</h1>
+          <div className='user-card-content'>
+            <div className='avatar-section'>
+              <img className='avatar' src={avatar_url} alt='avatar' />
+            </div>
+            <div className='name-of-user'>
+              <h3>{name}</h3>
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 };
