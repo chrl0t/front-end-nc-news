b8c65227e08a08852d3c8e02a9f019960fc1b65d
chrl0t <charlotte_i@live.co.uk>
2020-12-10 15:43:10 +0000

    added a welcome message to default logged in user

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     <header className='header'>
       <Link className='header' to={'/'}>
         <h1 className='header-title'>NC NEWS</h1>
+        <h3 className='header-welcome'>Welcome jessjelly...</h3>
       </Link>
     </header>
   );
